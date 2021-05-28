d211f1185d9dac6f6192d7140057b34777100923
Cyndi-Tokyotech <82554798+Cyndi-Tokyotech@users.noreply.github.com>
2021-05-28 22:10:24 +0900

    final report

--- 16_final_report/06_MPI+OpenMP+SIMD/mpi_openmp_simd_code.cpp
+++ 16_final_report/06_MPI+OpenMP+SIMD/mpi_openmp_simd_code.cpp
@@ -1,4 +1,5 @@
 //20M19118
+//module load intel-mpi
 //compile with mpicxx mpi_openmp_simd_code.cpp -fopenmp -march=native -O3 -std=c++11
 //mpirun -np 4 ./a.out
 
@@ -22,16 +23,12 @@ int main(int argc, char** argv)
     vector<float> subA(N * N / size);
     vector<float> subB(N * N / size);
     vector<float> subC(N * N / size, 0);
-    
-    for (int i = 0; i < N; i++)
-    {
-        for (int j = 0; j < N; j++)
-        {
-            A[N * i + j] = drand48();
-            B[N * i + j] = drand48();
-        }
+    for (int i=0; i<N; i++) {
+      for (int j=0; j<N; j++) {
+        A[N*i+j] = drand48();
+        B[N*i+j] = drand48();
+      }
     }
-    
     int offset = N / size * rank;
     for (int i = 0; i < N / size; i++)
         for (int j = 0; j < N; j++)
@@ -46,12 +43,11 @@ int main(int argc, char** argv)
     int n_chunks = N / 64;
     
     double comp_time = 0, comm_time = 0;
-    for (int irank = 0; irank < size; irank++)
-    {
+    for (int irank = 0; irank < size; irank++)  {
         auto tic = chrono::steady_clock::now();
-        
         offset = N / size * ((rank + irank) % size);
         int chunk, i, j, k;
+                
 # pragma omp parallel shared (subA, subB, subC, size, offset) private (chunk, i, j, k, columnSect)
 {
 # pragma omp for
@@ -97,7 +93,6 @@ int main(int argc, char** argv)
 
     MPI_Allgather(&subC[0], N * N / size, MPI_FLOAT, &C[0], N * N / size, MPI_FLOAT, MPI_COMM_WORLD);
   
-
     for (int i = 0; i < N; i++)
         for (int j = 0; j < N; j++)
             for (int k = 0; k < N; k++)
